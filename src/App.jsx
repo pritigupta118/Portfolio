66b90a03763350ecaed155090895464f07506ea2
Priti Gupta <civilstudent9900@gmail.com>
2024-02-01 12:31:36 +0530

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-
-
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import './index.css'
 
 function App() {
-  
   return (
-    <div className="bg-red-400 h-screen">hello</div>
+   <div className="bg-[#131313]">
+    <Header/>
+    <Outlet/>  
+  
+   </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Skills from './components/Skills.jsx'
+import Projects from './components/Projects.jsx'
+import Chat from './components/Chat.jsx'
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[
+      {path:"/",
+      element: <Home/>
+    },
+    {
+      path: "/about",
+      element: <About/>,
+    },
+    {
+      path: "/skills",
+      element: <Skills/>,
+    },
+    {
+      path: "/projects",
+      element: <Projects/>,
+    },
+    {
+      path: "/chat",
+      element: <Chat/>,
+    },
+
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ 
+    <RouterProvider router={appRouter}/>
+  
 )
